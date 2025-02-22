bac1c8400b94202204e0c1c35089a32940c057eb
TimsTittus <timstittus1@gmail.com>
2025-02-22 09:12:37 +0530

    chnages in speed,velocity,camera focus etc

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 120.0
+const JUMP_VELOCITY = -300.0
 const GRAVITY = 980.0  # Define gravity manually
 
 func _physics_process(delta: float) -> void:
